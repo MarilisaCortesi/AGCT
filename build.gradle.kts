a1c70fdba620dc81306fc598e6f27f371ec1853d
Danilo Pianini <danilo.pianini@unibo.it>
2019-09-16 17:13:53 +0200

    Improve build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val kotlinVersion = "1.3.41"
+val kotlinVersion = "1.3.50"
 val kotlinTestVersion = "3.3.3"
 
 plugins {
@@ -12,8 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    compile("org.jetbrains.kotlin:kotlin-scripting-jvm")
-    compile("org.slf4j:slf4j-nop:1.7.26")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm")
     testCompile("io.kotlintest:kotlintest-runner-junit5:$kotlinTestVersion")
 }
 
