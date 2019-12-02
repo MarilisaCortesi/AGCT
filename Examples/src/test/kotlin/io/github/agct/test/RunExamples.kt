5b7b23136a013d032ad50cd33abff99d2e17afe0
Danilo Pianini <danilo.pianini@unibo.it>
2019-12-02 18:35:34 +0100

    Tentative test

---
+++ Examples/src/test/kotlin/io/github/agct/test/RunExamples.kt
@@ -0,0 +1,18 @@
+package io.github.agct.test
+
+import io.github.classgraph.ClassGraph
+import io.kotlintest.specs.StringSpec
+
+internal class RunExamples : StringSpec({
+    ClassGraph().whitelistPackages("io.github.agct.examples")
+        .enableAllInfo()
+        .scan()
+        .allClasses
+        .map { it to it.getMethodInfo().find { it.isStatic && it.name == "main" } }
+        .filterNot { it.second == null }
+        .forEach { (clazz, main) ->
+            "Example in ${clazz.classpathElementFile.name} should not crash" {
+                main!!.loadClassAndGetMethod().invoke(null)
+            }
+        }
+})
